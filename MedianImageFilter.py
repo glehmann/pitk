77ec72a15aeb89c922ba876179a31b2f221b3da8
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2005-04-12 13:20:31 +0200

    add MedianImageFilter.py example
    darcs-hash:20050412112031-2fc9d-c744666c254edf26b9e7eaede479355979835434.gz

---
+++ MedianImageFilter.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#
+#  Example on the use of the MedianImageFilter
+#
+# this is a modified version which demonstrate use of itk module
+
+import itk
+from sys import argv
+
+# to clearly view that itk module (ie InsightToolkit in it) is really long to import :-(
+print 'modules loaded'
+
+# types and dimension "declaration"
+#
+# Types and dimension can come from command line. It can't be easily done in C++.
+# we should just do :
+# dim = argv[4]
+# outPType = inPType = argv[5]
+# 
+dim = 2
+outPType = inPType = 'UC'
+inType = inPType + str(dim)
+outType = outPType + str(dim)
+
+# use type to instanciate reader, writer and filter
+reader = itk.ImageFileReader[inType].New()
+writer = itk.ImageFileWriter[outType].New()
+filter  = itk.MedianImageFilter[inType+outType].New()
+
+filter.SetInput( reader.GetOutput() )
+writer.SetInput( filter.GetOutput() )
+
+reader.SetFileName( argv[1] )
+writer.SetFileName( argv[2] )
+
+# use type declared above to instanciate itk::Size object
+sizeRadius = itk.Size[dim]()
+sizeRadius.SetElement( 0, eval( argv[3] ) )
+sizeRadius.SetElement( 1, eval( argv[3] ) )
+
+filter.SetRadius( sizeRadius )
+
+
+writer.Update()
+
+
+
